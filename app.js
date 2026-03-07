4dbccd04f48d2762b40323fb2116aefb1dbc3c8f
Mohammad Nahid Uddin Babor <mdsnbabor828@gmail.com>
2026-03-08 00:30:24 +0600

    added signIn functionality

---
+++ app.js
@@ -0,0 +1,16 @@
+// Sign In page logic
+const signInBtn = document.getElementById('sign-in-btn');
+const userNameInput = document.getElementById('username-input');
+const passwordInput = document.getElementById('password-input');
+signInBtn.addEventListener('click',()=>{
+    if(userNameInput.value=='admin' && passwordInput.value=='admin123'){
+        alert("Sign In Success 👍");
+        window.location.assign('home.html');
+    }
+    else{
+        alert("Sign In Failed 😭");
+    }
+});
+
+// Home page logic
+
