0a0be8fd7260af1d992c2b0c0da1d9efa640abb4
Mohammad Nahid Uddin Babor <mdsnbabor828@gmail.com>
2026-03-08 21:30:28 +0600

    added search to find functionality

--- script/app.js
+++ script/app.js
@@ -24,78 +24,112 @@ const loadIssuesCard = async (status) => {
 loadIssuesCard("all");
 
 filterBtns.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      const status = btn.innerText.toLowerCase();
-      loadIssuesCard(status);
-    });
+  btn.addEventListener("click", () => {
+    const status = btn.innerText.toLowerCase();
+    loadIssuesCard(status);
   });
+});
 
-const createElements =(arr)=>{
-    const htmlEliments = arr.map((el)=>`
+const createElements = (arr) => {
+  const htmlEliments = arr.map(
+    (el) => `
      <p class="px-4 py-1 text-[12px] rounded-full 
-          ${el== "bug" ? "text-[#EF4444] bg-[#FEECEC] border-[#EF4444] border-1" 
-          : el == "help wanted" ? "bg-[#FFF8DB] text-[#D97706] border-[#D97706] border-1" 
-          : "bg-[#BBF7D0] text-[#00A96E] border-[#00A96E] border-1"} flex 
-          items-center justify-center">
-      <img src="${
-          el === "bug"
-          ? "./assets/Bug.png": el=="help wanted" 
-          ? "./assets/Help.png": "./assets/Enhance.png"
-          }" alt="" />
-          ${el.toUpperCase()}
-          </p>`);
-    return htmlEliments.join(" ");
-};
+              flex items-center justify-center gap-1
+          ${
+            el == "bug"
+              ? "text-[#EF4444] bg-[#FEECEC] border-[#EF4444] border"
+              : el == "help wanted"? 
+              "bg-[#FFF8DB] text-[#D97706] border-[#D97706] border"
+              : "bg-[#BBF7D0] text-[#00A96E] border-[#00A96E] border"
+          } ">
 
+      <img src="${
+            el === "bug"
+            ? "./assets/Bug.png"
+            : el == "help wanted"
+            ? "./assets/Help.png"
+            : "./assets/Enhance.png"
+            }" alt="" />
+            ${el.toUpperCase()}
+            </p>`,
+    );
+
+  return htmlEliments.join(" ");
+};
 
 const displayCard = (cardData, status) => {
   issuesCardContainer.innerHTML = "";
-  if (status === "open") {
-    filteredData = cardData.filter((data) => data.status === "open");
-    console.log(filteredData);
-  } else if (status === "closed") {
-    filteredData = cardData.filter((data) => data.status === "closed");
-  } else {
-    filteredData = cardData;
-  }
-  filteredData.forEach((data) => {
-    const cardContent = document.createElement("div");
 
-    cardContent.innerHTML = `
-      <div class="card h-full shadow-sm bg-white p-3 space-y-4 border-t-3 ${
-        data.status === "open" ? "border-[#00A96E]" : "border-[#A855F7]"
-      }">
-        <div class="flex justify-between">
+      if (status === "open") {
+        filteredData = cardData.filter((data) => data.status === "open");
+      } else if (status === "closed") {
+       filteredData = cardData.filter((data) => data.status === "closed");
+      } else {
+        filteredData = cardData;
+      }
+
+  filteredData.forEach((data) => {
+      const cardContent = document.createElement("div");
+
+      cardContent.innerHTML = `
+        <div class="card h-full shadow-sm bg-white p-3 space-y-4 border-t-3 
+          ${data.status === "open" ? "border-[#00A96E]" : "border-[#A855F7]"
+          }">
+
+          <div class="flex justify-between">
           <img src="${
             data.status === "open"
               ? "./assets/Open-Status.png"
-              : "./assets/Closed.png"
-          }" alt="" />
-          <p class="px-4 rounded-full ${data.priority == "high" ? "text-[#EF4444] bg-[#FEECEC]" : data.priority == "medium" ? "bg-[#FFF8DB] text-[#D97706]" : "bg-[#EEEFF2] text-[#9CA3AF]"}
-        ">${data.priority}</p>
-        </div> 
+              : "./assets/Closed.png"}"
+           alt="" />
 
-        <h4 class="title text-sm font-semibold">
-          ${data.title}
-        </h4>
+          <p class="px-4 rounded-full ${
+              data.priority == "high"
+              ? "text-[#EF4444] bg-[#FEECEC]"
+              : data.priority == "medium"? 
+              "bg-[#FFF8DB] text-[#D97706]"
+              : "bg-[#EEEFF2] text-[#9CA3AF]"
+          }">
+              ${data.priority.toUpperCase()}
+          </p>
+          </div>
 
-        <p class="text-[12px] text-gray-500">
-          ${data.description}
-        </p>
-        <div class="flex flex-row sm:flex-col lg:flex-row gap-2">
+          <h4 class="title text-sm font-semibold">
+              ${data.title}
+          </h4>
 
-           ${createElements(data.labels)}
+          <p class="text-[12px] text-gray-500">
+              ${data.description}
+          </p>
 
+          <div class="flex flex-row sm:flex-col lg:flex-row gap-2">
+              ${createElements(data.labels)}
+          </div>
+
+          <hr class="border-gray-300" />
+
+          <p class="text-[12px] text-gray-500">#${data.id} by ${data.author}</p>
+          <p class="text-[12px] text-gray-500">${data.createdAt}</p>
         </div>
+      `;
 
-        <hr class="border-gray-300" />
-
-        <p class="text-[12px] text-gray-500">#${data.id} by ${data.author}</p>
-        <p class="text-[12px] text-gray-500">${data.createdAt}</p>
-
-      </div>
-    `;
-    issuesCardContainer.appendChild(cardContent);
-    count.innerText = filteredData.length;
-  });
+      issuesCardContainer.appendChild(card);
+    });
+      count.innerText = filteredData.length;
 };
+
+document.getElementById("search-btn").addEventListener("click", () => {
+  const input = document.getElementById("search-input");
+  const searchValue = input.value.trim().toLowerCase();
+  fetch(
+    `https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${searchValue}`,
+  )
+    .then((res) => res.json())
+    .then((json) => {
+      const allcard = json.data;
+      const filtercard = allcard.filter((card) =>
+        card.title.toLowerCase().includes(searchValue),
+      );
+      displayCard(filtercard);
+    });
+});
